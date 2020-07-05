0ea1091f466e1691ac98944dd988164a2861d554
Ossama <jlididev20@gmail.com>
2020-07-05 19:43:56 +0200

    Updates

---
+++ precache-manifest.5d29f72c0f0f6f666b01d5d9f46ccb1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e8a981a54514015ca4007126137cc51",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "8b714f5033b9c43c6132",
+    "url": "/covid19/static/css/main.5528375e.chunk.css"
+  },
+  {
+    "revision": "32e9dc3e6d5e96e2adc7",
+    "url": "/covid19/static/js/2.3613378d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19/static/js/2.3613378d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b714f5033b9c43c6132",
+    "url": "/covid19/static/js/main.bb535620.chunk.js"
+  },
+  {
+    "revision": "636bdfdab0a46f674c1f",
+    "url": "/covid19/static/js/runtime-main.09a13a41.js"
+  }
+]);
