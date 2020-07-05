2625d69cec2476cef98140d2bbcaeb18c975511b
Ossama <jlididev20@gmail.com>
2020-07-05 20:34:25 +0200

    delete many images

--- public/images/Corona dan Rangkaian Hukum di Sekitarnya - hukumonline.com_files/namespace.js
+++
@@ -1,2 +0,0 @@
-String.prototype.namespace=function(separator){var ns=this.split(separator||'.'),o=window,i,len;for(i=0,len=ns.length;i<len;i++){o=o[ns[i]]=o[ns[i]]||{};}
-return o;};String.prototype.isClassExist=function(){return eval('typeof '+this)=='function';};
