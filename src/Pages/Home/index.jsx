2742006ba3f85d28dfdafedd34255b476467ed6b
Ossama <jlididev20@gmail.com>
2020-07-06 00:08:48 +0200

    Create navbar with heading and about, symptons & treaitment covid to home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 //Import All Components Section Page Home
 import Header from '../../Components/header';
-
+import About from '../../Components/about';
+import Symptons from '../../Components/symptons';
+import Treatment from '../../Components/treatment'
 
 function Home(props) {  
     
     return (
         <React.Fragment>
             <Header data = {props.data} />
+            <About />
+            <Symptons />
+            <Treatment />
         </React.Fragment>
     );
 }
